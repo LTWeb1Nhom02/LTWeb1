73353382d26cffcdfbb94e16d3f546a27a264281
ThaiMinhThong <33929472+ThaiMinhThong@users.noreply.github.com>
2017-11-29 23:09:55 +0700

    content đăng nhập

---
+++ Content_Danhnhap.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div class="container">
+		<div class="account">
+			<h2 class="account-in" style="width: 1130px">Đăng nhập</h2>
+				<form class="text-center">
+				<div>
+					<span>Username</span>
+					<input type="text" placeholder="Tên đăng nhập" style="width: 200px" required autofocus>
+				</div> 	
+				<div>			
+					<span class="name-in">Password</span>
+					<input type="password" placeholder="*********" style="width: 200px" required> 
+				</div>			
+				<div>				
+					<input type="submit" value="Login" style="width: 100px">
+				</div> 
+
+				</form>
+		</div>
+	</div>
+</body>
+</html>
