ffbd5214bf0b485d4b25687b03625157b03c2628
truongthuan97 <33859797+truongthuan97@users.noreply.github.com>
2017-12-06 22:32:04 +0700

    conten trang chủ đã xử lý giao diện và code đổ dữ liệu

---
+++ Content_Trangchu.php
@@ -0,0 +1,353 @@
+<?php
+	require_once "./lib/db.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div class="container">
+			<div class="content">
+				<!-- menuleft -->
+					<?php include 'LayoutGeneral/menu.php'; ?>
+
+
+				<!-- menuright -->
+					<div class="col-md-9 top-single">
+						<div class="content-bottom" style="margin-top: 5px">
+						<h3 class="future" style="width: 925px">MỚI NHẤT</h3>
+						<div class="content-bottom-in">
+							<ul id="flexiselDemo0">			
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img src="images/banner4.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px; padding: 17px">Đặt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>						
+								</li>
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/banner4.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/pic10.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>							
+								</li>
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/pic10.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>					
+								</li>
+					
+							</ul>
+					<script type="text/javascript">
+		$(window).load(function() {
+			$("#flexiselDemo0").flexisel({
+				visibleItems: 3,
+				animationSpeed: 1000,
+				autoPlay: true,
+				autoPlaySpeed: 3000,    		
+				pauseOnHover: true,
+				enableResponsiveBreakpoints: true,
+		    	responsiveBreakpoints: { 
+		    		portrait: { 
+		    			changePoint:480,
+		    			visibleItems: 1
+		    		}, 
+		    		landscape: { 
+		    			changePoint:640,
+		    			visibleItems: 2
+		    		},
+		    		tablet: { 
+		    			changePoint:768,
+		    			visibleItems: 3
+		    		}
+		    	}
+		    });
+		    
+		});
+	</script>
+	<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+						</div>
+					</div>
+						<div class="content-bottom" style="margin-top: 5px">
+						<h3 class="future" style="width: 925px">BÁN CHẠY</h3>
+						<div class="content-bottom-in">
+							<ul id="flexiselDemo1">			
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/pic10.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>						
+								</li>
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/pic10.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>	
+								</li>
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/pic10.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>							
+								</li>
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/pic10.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>				
+								</li>
+					
+							</ul>
+					<script type="text/javascript">
+		$(window).load(function() {
+			$("#flexiselDemo1").flexisel({
+				visibleItems: 3,
+				animationSpeed: 1000,
+				autoPlay: true,
+				autoPlaySpeed: 3000,    		
+				pauseOnHover: true,
+				enableResponsiveBreakpoints: true,
+		    	responsiveBreakpoints: { 
+		    		portrait: { 
+		    			changePoint:480,
+		    			visibleItems: 1
+		    		}, 
+		    		landscape: { 
+		    			changePoint:640,
+		    			visibleItems: 2
+		    		},
+		    		tablet: { 
+		    			changePoint:768,
+		    			visibleItems: 3
+		    		}
+		    	}
+		    });
+		    
+		});
+	</script>
+	<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+						</div>
+					</div>
+						<div class="content-bottom" style="margin-top: 5px">
+						<h3 class="future" style="width: 925px">XEM NHIỀU</h3>
+						<div class="content-bottom-in">
+							<ul id="flexiselDemo2">			
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/pic10.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>						
+								</li>
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/pic10.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>	
+								</li>
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/pic10.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>							
+								</li>
+								<li>
+									<div class="col-md men">
+										<a href="" class="compare-in"><img  src="images/pic10.jpg" alt="" height="200px"/>	
+										<div class="compare" style="width: 198px;height: 190px">
+											<span style="width: 100px">Giỏ hàng</span>
+											<span style="width: 100px">Đăt mua</span>
+										</div>
+										</a>
+										<div class="top-content">
+											<h5>Mascot Kitty - White</h5>
+											<div class="white">
+												<a href="chitiet.php" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">Chi tiết</a>
+												<p class="dollar"><span class="in-dollar" style="font-size: 10px"></span><span>16.000.000</span></span></p>
+												<div class="clearfix"></div>
+											</div>
+										</div>
+									</div>					
+								</li>
+					
+							</ul>
+					<script type="text/javascript">
+		$(window).load(function() {
+			$("#flexiselDemo2").flexisel({
+				visibleItems: 3,
+				animationSpeed: 1000,
+				autoPlay: true,
+				autoPlaySpeed: 3000,    		
+				pauseOnHover: true,
+				enableResponsiveBreakpoints: true,
+		    	responsiveBreakpoints: { 
+		    		portrait: { 
+		    			changePoint:480,
+		    			visibleItems: 1
+		    		}, 
+		    		landscape: { 
+		    			changePoint:640,
+		    			visibleItems: 2
+		    		},
+		    		tablet: { 
+		    			changePoint:768,
+		    			visibleItems: 3
+		    		}
+		    	}
+		    });
+		    
+		});
+	</script>
+	<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+						</div>
+					</div>
+						
+					</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+	
+</body>
+</html>
