cd1b023206d8a9c1654b1900e37b606c2aef1efe
TranMinhSon <33929589+TranMinhSon@users.noreply.github.com>
2017-12-06 22:40:16 +0700

    giao diện content đăng kí

---
+++ Content_Danhky.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+
+</head>
+<body>
+	<div class="container">
+		<div class="account">
+			<h2 class="account-in">Đăng ký</h2>
+				<form class="text-center">
+				<div>
+					<span >Full name<b style="color: red">*</b></span>
+					<input type="text" placeholder="Họ và tên" style="margin-left: 8.5px; width: 400px" required autofocus>
+				</div> 	
+					
+				<div> 	
+					<span class="mail">Email<b style="color: red">*</b></span>
+					<input type="text" placeholder="Email" style="margin-left: 4px; width: 400px" required> 
+				</div>
+				<div>			
+					<span class="name-in">User name<b style="color: red">*</b></span>
+					<input type="text" placeholder="Tên đăng nhập" style="width: 400px;margin-left: 0.5px" required> 
+				</div>	
+				<div> 
+					<span class="word">Password<b style="color: red">*</b></span>
+					<input type="password" placeholder="********" style="margin-left:7px; width: 400px" required>
+				</div>
+				<div> 
+					<span class="word">Confirm<b style="color: red">*</b></span>
+					<input type="password" placeholder="********" style="margin-left:15.5px; width: 400px" required>
+				</div>
+				<div>
+					<input type="submit" value="Registration" style="width: 250px" required> 
+				</div>				
+					
+				</form>
+		</div>
+	</div>
+</body>
+</html>
