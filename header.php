8a9a2378cb81a8d818c69ebcb8253771694fff2e
truongthuan97 <33859797+truongthuan97@users.noreply.github.com>
2017-12-14 22:25:32 +0700

    header đã xử lý code php hoàn chỉnh

--- header.php
+++ header.php
@@ -1,5 +1,54 @@
-<?php
-	require_once "./lib/db.php";
+
+<?php 
+	session_start();
+	require_once './lib/db.php';
+
+	if (!isset($_SESSION["cart"])) {
+	$_SESSION["cart"] = array();
+}
+
+function add_item($proId, $q) {
+	if (array_key_exists($proId, $_SESSION["cart"])) {
+		$_SESSION["cart"][$proId] += $q;
+	} else {
+		$_SESSION["cart"][$proId] = $q;
+	}
+}
+
+function delete_item($proId) {
+	unset($_SESSION["cart"][$proId]);
+}
+
+function update_item($proId, $q) {
+	$_SESSION["cart"][$proId] = $q;
+}
+
+function get_total_items() {
+	$total = 0;
+	foreach ($_SESSION["cart"] as $proId => $q) {
+		$total += $q;
+	}
+
+	return $total;
+}
+
+	if (!isset($_SESSION["dang_nhap_chua"])) {
+		$_SESSION["dang_nhap_chua"] = 0;
+	} 
+
+	if ($_SESSION["dang_nhap_chua"] == 0) {
+		if(isset($_COOKIE["auth_user_id"])) {
+			// tái tạo session
+			$user_id = $_COOKIE["auth_user_id"];
+			$sql = "select * from users where f_ID = $user_id";
+			$rs = load($sql);
+			$_SESSION["current_user"] = $rs->fetch_object();
+			//$_SESSION["dang_nhap_chua"] = 1;
+		}
+		// } else {
+		// 	header("Location: dangnhap.php");
+		// }
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,11 +68,26 @@
 				</div>
 				<div class="header-in" style="margin-right: -70px">
 					<ul class="icon1 sub-icon1">
-							<li  ><a href="dangnhap.php">ĐĂNG NHẬP</a> </li>
-							<li  ><a href="dangky.php">  ĐĂNG KÝ</a></li>
-							<li ><a href="#" >GIỎ HÀNG</a></li>
-							<li > <a href="#" >HOSTLINE</a> </li>	
-							
+						<?php
+							if ($_SESSION["dang_nhap_chua"] == 0) { 
+						 ?>
+						 		<li  ><a href="dangnhap.php">  ĐĂNG NHẬP</a></li>
+								<li  ><a href="dangky.php">  ĐĂNG KÝ</a></li>
+								<li ><a href="view_cart.php" >GIỎ HÀNG</a></li>
+								<li > <a href="#" >HOSTLINE</a> </li>	
+						<?php
+							} 
+						 ?>
+
+						 <?php
+							if ($_SESSION["dang_nhap_chua"] != 0) { 
+						 ?>
+								<li  ><a href="dangky.php">  ĐĂNG KÝ</a></li>
+								<li ><a href="view_cart.php" >GIỎ HÀNG</a></li>
+								<li > <a href="#" >HOSTLINE</a> </li>	
+						<?php
+							} 
+						 ?>
 					</ul>
 				</div>
 				<div class="clearfix"> </div>
@@ -44,7 +108,7 @@
 						$rs = load($sql);
 						while ($row = $rs->fetch_assoc()) :
 						?>
-							<li><a href="#"><?= $row["CatName"] ?></a></li>
+							<li><a href="product.php?id=<?= $row["CatID"] ?>"><?= $row["CatName"] ?></a></li>
 							
 						<?php
 						endwhile;
@@ -66,14 +130,29 @@
 						</ul>
 					</li> 							
 					<li><a href="#" >  Tin tức</a></li>            
-					<li><a href="#" >Trợ giúp</a></li>						  				 
-					<li style="float: right; margin-right: -70px"><a href="#" >Tài khoản</a>
-						<ul class="drop">
-							<li><a href="#">Thông tin cá nhân</a></li>
-							<li><a href="#">Đổi mật khẩu</a></li>
-							<li><a href="#"> Thoát</a></li>
-						</ul>
-					</li> 
+					<li><a href="#" >Trợ giúp</a></li>		
+					<?php 
+						if ($_SESSION["dang_nhap_chua"] != 0) {
+					?>
+							<li style="float: right; margin-right: -70px"><a href="#" ><?= $_SESSION["current_user"]->f_Name ?></a>
+								<ul class="drop">
+									<li><a href="#">Thông tin cá nhân</a></li>
+									<li><a href="#">Đổi mật khẩu</a></li>
+									<li><a href="LayoutPrivate/logout.php"> Thoát</a></li>
+								</ul>
+							</li> 		
+					<?php 
+						}
+					?>
+
+					 <?php 
+						if ($_SESSION["dang_nhap_chua"] == 0) {
+					?>
+						
+					<?php 
+						}
+					?>				  				 
+					
 				</ul>
 				<script type="text/javascript" src="js/nav.js"></script>
 			</div>
