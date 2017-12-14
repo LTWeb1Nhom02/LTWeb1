9986582af11f1a8f809c974561a79b17c6f2cbbb
ThaiMinhThong <33929472+ThaiMinhThong@users.noreply.github.com>
2017-12-14 22:42:17 +0700

    trang đăng kí hoàn chỉnh giao diện

---
+++ dangky.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include 'LayoutGeneral/linkketnoi.php'; ?>
+</head>
+<body>
+	<?php include 'LayoutGeneral/header.php'; ?>
+	<?php include 'LayoutPrivate/Content_Danhky.php'; ?>
+	<?php include 'LayoutGeneral/flooter.php'; ?>
+</body>
+</html>
