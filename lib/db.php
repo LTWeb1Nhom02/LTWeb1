e24133eed9eb204f87f17961ef1eecc2fea6f75c
truongthuan97 <33859797+truongthuan97@users.noreply.github.com>
2017-12-14 22:07:08 +0700

    file kết nối cơ sở dử liệu update

---
+++ lib/db.php
@@ -0,0 +1,30 @@
+<?php
+
+define("HOST", "127.0.0.1");
+define("DB", "qlbh");
+define("UID", "root");
+define("PWD", "");
+
+// Đọc dữ liệu
+function load($sql) {
+	$cn = new mysqli(HOST, UID, PWD, DB);
+	if ($cn->connect_errno) {
+	    die("FAILED");
+	}
+
+	$cn->query("set names 'utf8'");
+	$rs = $cn->query($sql);
+	return $rs;
+}
+
+// Ghi dữ liệu
+function write($sql) {
+	$cn = new mysqli(HOST, UID, PWD, DB);
+	if ($cn->connect_errno) {
+	    die("FAILED");
+	}
+
+	$cn->query("set names 'utf8'");
+	$cn->query($sql);
+	
+}
