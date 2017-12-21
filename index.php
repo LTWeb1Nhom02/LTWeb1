dccc16faa5689365607bac1206742f4e92d2d324
truongthuan97 <33859797+truongthuan97@users.noreply.github.com>
2017-12-21 23:00:59 +0700

    giao diện hoàn chỉnh trang chủ

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	require_once "lib/db.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include 'LayoutGeneral/linkketnoi.php'; ?>
+</head>
+<body>
+	<?php include 'LayoutGeneral/header.php'; ?>
+	<?php include 'LayoutGeneral/slideshow.php'; ?>
+	<?php include 'LayoutPrivate/Content_Trangchu.php'; ?>
+	<?php include 'LayoutGeneral/flooter.php'; ?>
+
+</body>
+</html>
