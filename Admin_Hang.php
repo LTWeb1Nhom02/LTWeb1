c1d70a17be05c37c9d8f3e490d1a323d542fb760
truongthuan97 <33859797+truongthuan97@users.noreply.github.com>
2018-01-09 22:57:49 +0700

    hoàn chỉnh trang admin quản lý hãng

---
+++ Admin_Hang.php
@@ -0,0 +1,22 @@
+<?php
+	require_once "./lib/db.php";
+	require_once 'cart.inc';
+	if ($_SESSION["dang_nhap_chua"] !=2) {
+		header('Location:index.php');
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+	<?php include 'LayoutGeneral/linkketnoi.php'; ?>
+	
+</head>
+<body>
+	<?php include 'LayoutGeneral/header.php'; ?>
+	<?php include 'Trang_Admin/Content_AdminHang.php'; ?>
+	<?php include 'LayoutGeneral/flooter.php'; ?>
+</body>
+</html>
